c955df312cb81ee357c84d77c5644dc8c2e1e5b1
seanmcmahon101 <112512952+seanmcmahon101@users.noreply.github.com>
2024-06-25 12:10:58 +0100

    Create CoDate 2 -x Scraper.py

---
+++ CoDate 2 -x Scraper.py
@@ -0,0 +1,52 @@
+def codedatescraper():
+    try:
+        codate_url = "http://hffsuk02/Reports/report/ReportsUK/Customer/CoDate2-X"
+        logger.debug(f"Starting codedatescraper with URL: {codate_url}")
+        options = configure_options()
+        driver = webdriver.Chrome(options=options)
+        wait = WebDriverWait(driver, 20)
+
+        driver.get(codate_url)
+        driver.fullscreen_window()
+        time.sleep(10)
+
+        frames = driver.find_elements(By.TAG_NAME, "iframe")
+        if frames:
+            driver.switch_to.frame(frames[0])
+            logger.debug("Navigated to frame")
+
+        dropdown_button = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, "#ReportViewerControl_ctl05_ctl04_ctl00_ButtonImg")))
+        driver.execute_script("arguments[0].scrollIntoView(true);", dropdown_button)
+        time.sleep(5)
+        driver.execute_script("arguments[0].click();", dropdown_button)
+        time.sleep(10)
+
+        excel_download_button = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, "#ReportViewerControl_ctl05_ctl04_ctl00_Menu > div:nth-child(2) > a")))
+        driver.execute_script("arguments[0].scrollIntoView(true);", excel_download_button)
+        time.sleep(1)
+        driver.execute_script("arguments[0].click();", excel_download_button)
+        time.sleep(15)
+        logger.info("Download initiated")
+
+        latest_file = None
+        attempt_time = 0
+        while not latest_file and attempt_time < 60:
+            logger.debug(f"Attempt {attempt_time + 1}: Checking for the latest file")
+            latest_file = get_latest_file_path(downloads_dir)
+            time.sleep(1)
+            attempt_time += 1
+
+        driver.quit()
+
+        if latest_file:
+            logger.info(f"File downloaded successfully: {latest_file}")
+            df = pd.read_excel(latest_file)
+            return df
+        else:
+            logger.warning("No new file was downloaded")
+            return None
+    except Exception as e:
+        logger.error(f"Error in codedatescraper: {e}", exc_info=True)
+        if 'driver' in locals():
+            driver.quit()
+        return None
